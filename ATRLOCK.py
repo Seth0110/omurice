8b11aca2b5173287aa65b9d089ac21078f5d001c
Seth <seth.sevier@gmail.com>
2018-02-01 14:37:28 -0500

    ATRLOCK aaaaalmost done

--- ATRLOCK.py
+++ ATRLOCK.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+pp#!/usr/bin/python3
 
 # Copyright 2018 Seth Sevier
 
@@ -10,7 +10,11 @@
 
 ### Python3 port of ATRLOCK.PAS ###
 # TODO:
+# What is btrim()???
 # Figure out how check if a filename is valid().
+# Figure out where the read in line disappears, in encode() somewhere
+# Should encode() be adding spaces between all the characters like it currently is?
+# Find out of lstr() does what I expect
 
 import os, pdb, random, sys, time
 
@@ -19,20 +23,27 @@ from subprocess import call
 call(['rm','SNIPER.ATL']) 
 
 LOCKTYPE = 3
+lock_code = 0
+lock_pos = 0
+lock_dat = 0
 
 def encode(s):
+    global lock_pos
+    global lock_code
+    global lock_dat
     if lock_code != '':
         for i in s:
             lock_pos += 1
             if lock_pos > len(lock_code):
-                lock_pos = 1
+                lock_pos = 0
             if ((ord(i) in range(0,31)) or (ord(i) in range(128,255))):
                 this_dat = ord(i) & 15
-                i = chr((ord(i) | (ord(lock_code[lock_pos]) | lock_dat)) + 1)
+                i = chr((ord(i)) ^ ord(lock_code[lock_pos % len(lock_code)]) ^ lock_dat + 1)
                 lock_dat = this_dat
     return s
 
-def prepare(s,s1):
+def prepare(s1):
+    s = ''
     # remove comments
     if (len(s1) == 0) or (s1[0] == ';'):
         s1 = ''
@@ -43,21 +54,20 @@ def prepare(s,s1):
                 k = i
             if k > 0:
                 s1 = s1[k - 1] ### This was originally lstr(s1,k - 1) 
-    # remove exess spaces
+    # remove excess spaces
     s2 = ''
     for i in range(0,len(s1)):
         if not (i in [' ','\b','\t','\n',',']):
             s2 = s2 + s1[i]
-        else:
             if s2 != '':
-                s = s + s2 + ' '
+                s = s + s2 # originally also added + ' '
                 s2 = ''
+    if s2 != '':
+        s = s + s2
     return s
 
-def write_line(s,s1):
-    print('write_line(' + s + ',' + s1 + ')')
-    s = prepare(s,s1)
-    
+def write_line(s1):
+    s = prepare(s1)
     if len(s) > 0:
         s = encode(s)
         f2.write(s + '\n')
@@ -69,14 +79,14 @@ lock_dat = 0
 if (len(sys.argv) < 2) | (len(sys.argv) > 4):
     print('Usage: ATRLOCK <robot[.at2]> [locked[.atl]]');
     sys.exit(0)
-fn1 = sys.argv[1]
+fn1 = sys.argv[1] # Needs a btrim()
 if not fn1.endswith('.AT2'):
     fn1 = fn1 + '.AT2'
 if not os.path.isfile(fn1):
     print('Robot "' + fn1 + '" not found!')
     sys.exit(0)
 if len(sys.argv) == 3:
-    fn2 = sys.argv[2]
+    fn2 = sys.argv[2] # Needs a btrim()
 else:
     fn2 = os.path.splitext(fn1)[0] + '.ATL'
 if not True: # should be a properly functioning valid(fn2):
@@ -91,11 +101,12 @@ f2 = open(fn2,'x')
 
 # copy comment header
 f2.write(';------------------------------------------------------------------------------\n')
-s = f1.readline()
-while len(s) > 0:
+
+for s in f1:
     if s[0] == ';':
         f2.write(s)
-    s = f1.readline()
+        s = f1.readline()
+    else: break
 
 # lock header
 f2.write(';------------------------------------------------------------------------------\n')
@@ -112,18 +123,18 @@ for i in lock_code:
     i = chr(ord(i) - 65)
 
 print('Encoding "' + fn1 + '"...')
+
+
 # encode robot
 if len(s) > 0:
-    print('s > 0')
-    write_line('',s.upper())
+    write_line(s.upper())
 f1.seek(0)
 for s1 in f1:
     # read line
     s1 = f1.readline()
-    s = ''
     s1 = s1.upper() # still needs a btrim function
     # write line
-    write_line(s,s1)
+    write_line(s1)
 
 print('Done. Used LOCK Format #' + str(LOCKTYPE) + '.')
 print('Only ATR2 v2.08 or later can decode.')
